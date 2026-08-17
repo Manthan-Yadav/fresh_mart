97868127e1268f79eb4ae9261f6d8a9e45d07497
Manthan-Yadav <manthanyadav309@gmail.com>
2026-08-17 16:57:16 +0530

    refactor: update server port handling and improve API base URL configuration

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const connectDB = require('./src/db/db');
 const dotenv = require('dotenv');
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
+
 connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  })
-})
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
 
 
